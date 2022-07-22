d034fe49357ad83ecc7809361103e023fa3fa253
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2022-07-22 12:18:10 -0400

    c

--- scripts/DownloadPlayers.py
+++ scripts/DownloadPlayers.py
@@ -10,26 +10,27 @@ with open("players.jsonl", "w") as file:
     error_count = 0
     player_count = 0
 
-    while player_count < 100_000:
+    while player_count < 20_000:
         url = base_url + str(page)
 
         try:
             response = json.loads(urllib.request.urlopen(url).read().decode())
 
-            columns = response["data"]["columns"]
-            file.write(",".join(columns) + "\n")
+            if response["status"] != "error":
+                columns = response["data"]["columns"]
+                file.write(",".join(columns) + "\n")
 
-            players = response["data"]["entries"]
-            for player in players:
-                file.write(json.dumps(player) + "\n")
-                player_count += 1
+                players = response["data"]["entries"]
+                for player in players:
+                    file.write(json.dumps(player) + "\n")
+                    player_count += 1
 
-            print("downloaded " + str(player_count) + " players")
-            time.sleep(0.5)
-            page += 1
+                print("downloaded " + str(player_count) + " players")
+                time.sleep(0.5)
+                page += 1
+            else:
+                error_count += 1
+                print("Errors_so_far: {}. Failed loading players on page {}. Response: {}".format(str(error_count), str(page), response))
         except Exception as e:
             error_count += 1
-            print("errors_so_far: " + error_count + ". Failed loading players " + page + "due to error: " + e)
-
-
-
+            print("Total error count: {}. Failed loading players on page {} due to error {}".format(str(error_count), str(page), str(e)))
