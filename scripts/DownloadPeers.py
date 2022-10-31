00d92f6e683ab1773e1ef0d9fb98d4e5f21fae09
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2022-10-31 16:56:41 -0400

    c

--- scripts/DownloadPeers.py
+++ scripts/DownloadPeers.py
@@ -51,7 +51,7 @@ def fetch_peer(username):
 #
 def fetch_peer_matches(peer, curr_page):
     time.sleep(3 + randrange(2))
-    query_url = '{}&next={}'.format(base_url.format(parse.quote_plus(peer)), parse.quote_plus(str(curr_page)))
+    query_url = '{}&next={}'.format(base_url.format(parse.quote(peer)), parse.quote_plus(str(curr_page)))
     print('{}| Peer query: {}'.format(current_timestamp(), query_url))
 
     errors = []
