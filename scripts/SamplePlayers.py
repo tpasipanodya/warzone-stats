761c2c8db75de03888fdd683405f90a3158dc1a9
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2022-11-20 11:54:07 -0500

    c

--- scripts/SamplePlayers.py
+++ scripts/SamplePlayers.py
@@ -8,7 +8,7 @@ from nordvpn_switcher import initialize_VPN, rotate_VPN, terminate_VPN
 
 page_size = 1
 browser = webdriver.Chrome()
-base_url = 'https://api.tracker.gg/api/v1/warzone/standard/leaderboards?type=battle-royale&platform=atvi&board=Wins&skip={}&take={}'
+base_url = 'https://api.tracker.gg/api/v1/warzone/standard/leaderboards?type=battle-royale&platform=psn&board=Wins&skip={}&take={}'
 opening_response_tag = '<html><head><meta name="color-scheme" content="light dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">'
 closing_response_tag = '</pre></body></html>'
 
