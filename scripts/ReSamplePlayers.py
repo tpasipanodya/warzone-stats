a20047c22dd6de7b592f6fcd10da15d44940b17e
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2022-12-11 16:06:07 -0500

    c

---
+++ scripts/ReSamplePlayers.py
@@ -0,0 +1,44 @@
+import json
+from random import randrange
+import time
+import datetime
+from retry import retry
+from selenium import webdriver
+from nordvpn_switcher import initialize_VPN, rotate_VPN, terminate_VPN
+
+
+def log(message):
+    print("{}| {}".format(current_timestamp(), message))
+
+
+def current_timestamp():
+    return datetime.datetime.now().isoformat()
+
+
+peer_ids = set()
+
+with open('../data/peers.processed.jsonl', 'r') as peers_file:
+    for peer_str in peers_file.readlines():
+        if peer_str.startswith('{'):
+            peer_json = json.loads(peer_str)
+            if len(peer_json['matches']) > 0:
+                peer_ids.add(peer_json['username'].strip())
+
+
+def resample_players(n=5):
+    player_ids = list(peer_ids)
+    player_ids.sort()
+
+    i = 0
+    while i < n:
+        i += 1
+        player_index = randrange(0, len(player_ids))
+        player_id = player_ids[player_index]
+        log('Sampled player \'{}\''.format(player_id))
+
+        with open('../data/sampled_players.processed.jsonl', 'a') as players_processed_file:
+            players_processed_file.write('{}\n'.format(json.dumps({'id': player_id})))
+    log('Done!')
+
+
+resample_players()
